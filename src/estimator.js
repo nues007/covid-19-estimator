f05b522faddaa173a6f3ffad5437050eb4bbfcd8
nues1979 <seunmark06@gmail.com>
2020-04-10 15:02:26 +0100

    challenge2

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,16 @@
 const covid19ImpactEstimator = (data) => {
   let days = 0;
-
-  const average = data.region.avgDailyIncomePopulation;
+  let day = 0;
   const income = data.region.avgDailyIncomeInUSD;
   if (data.periodType === 'days') {
-    days = parseInt(data.timeToElapse / 3, 10);
+    days = Math.trunc(data.timeToElapse / 3);
+    day = data.timeToElapse;
   } else if (data.periodType === 'months') {
     days = data.timeToElapse * 10;
+    day = data.timeToElapse * 30;
   } else {
-    days = parseInt((data.timeToElapse * 7) / 3, 10);
+    days = Math.trunc((data.timeToElapse * 7) / 3);
+    day = data.timeToElapse * 7;
   }
   const rate = (2 ** days);
   return {
@@ -16,24 +18,26 @@ const covid19ImpactEstimator = (data) => {
     impact: {
       currentlyInfected: data.reportedCases * 10,
       infectionsByRequestedTime: data.reportedCases * 10 * rate,
-      severeCasesByRequestedTime: parseInt(data.reportedCases * 10 * rate * 0.15, 10),
-      hospitalBedsByRequestedTime: parseInt((data.totalHospitalBeds * 0.35)
-     - data.reportedCases * (10 * rate * 0.15), 10),
-      casesForICUByRequestedTim: parseInt(data.reportedCases * (10 * rate * 0.05), 10),
-      casesForVentilatorsByRequestedTime: parseInt(data.reportedCases * (10 * rate * 0.02), 10),
-      dollarsInFlight: parseInt((data.reportedCases * 10 * rate * average
-      * income) / data.timeToElapse, 10)
+      severeCasesByRequestedTime: Math.trunc(data.reportedCases * 10 * rate * 0.15),
+      hospitalBedsByRequestedTime: Math.trunc((data.totalHospitalBeds * 0.35)
+     - data.reportedCases * (10 * rate * 0.15)),
+      casesForICUByRequestedTim: Math.trunc(data.reportedCases * 10 * (rate * 0.05)),
+      casesForVentilatorsByRequestedTime: Math.trunc(data.reportedCases * 10 * (rate * 0.02)),
+      dollarsInFlight: Math.trunc((data.reportedCases * 10 * rate
+        * data.region.avgDailyIncomePopulation
+      * income) / day)
     },
     severeImpact: {
       currentlyInfected: data.reportedCases * 50,
       infectionsByRequestedTime: data.reportedCases * 50 * rate,
-      severeCasesByRequestedTime: parseInt(data.reportedCases * (50 * rate * 0.15), 10),
-      hospitalBedsByRequestedTime: parseInt((data.totalHospitalBeds * 0.35)
-     - data.reportedCases * (50 * rate * 0.15), 10),
-      casesForICUByRequestedTim: parseInt(data.reportedCases * (50 * rate * 0.05), 10),
-      casesForVentilatorsByRequestedTime: parseInt(data.reportedCases * 50 * rate * 0.02, 10),
-      dollarsInFlight: parseInt((data.reportedCases * 50 * rate * average
-      * income) / data.timeToElapse, 10)
+      severeCasesByRequestedTime: Math.trunc(data.reportedCases * (50 * rate * 0.15)),
+      hospitalBedsByRequestedTime: Math.trunc((data.totalHospitalBeds * 0.35)
+     - data.reportedCases * (50 * rate * 0.15)),
+      casesForICUByRequestedTim: Math.trunc(data.reportedCases * 50 * (rate * 0.05)),
+      casesForVentilatorsByRequestedTime: Math.trunc(data.reportedCases * 50 * (rate * 0.02)),
+      dollarsInFlight: Math.trunc((data.reportedCases * 50 * rate
+        * data.region.avgDailyIncomePopulation
+      * income) / day)
 
     }
   };
